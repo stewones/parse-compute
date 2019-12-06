535a34eeba77a02bf7706304de3b209ef7acb792
Stewan Silva <oi@stewan.io>
2019-12-05 22:50:09 -0300

    chore(): update

--- install.sh
+++ install.sh
@@ -66,19 +66,19 @@ tput setaf 2; echo 'Adding APP_ID and MASTER_KEY';
 sleep 2;
 tput sgr0
 APP_ID=`pwgen -s 24 1`
-sudo sed -i "s/appId: process.env.APP_ID || .*/appId: process.env.APP_ID || '$APP_ID',/" ~/$APP_NAME/index.js
-sudo sed -i -e "s/APP_ID/$APP_ID/" "~/$APP_NAME/parse-dashboard-config.json"
-sudo sed -i -e "s/APP_NAME/$APP_NAME/" "~/$APP_NAME/parse-dashboard-config.json"
-sudo sed -i -e "s/DOMAIN/$DOMAIN/" "~/$APP_NAME/parse-dashboard-config.json"
-sudo sed -i -e "s/APP_NAME/$APP_NAME/" "~/$APP_NAME/dashboard-running.json"
-sudo sed -i -e "s/localhost:1337/app.$DOMAIN/" "~/$APP_NAME/index.js"
-sudo sed -i -e "s/http/https/" "~/$APP_NAME/index.js"
+sudo sed -i "s/appId: process.env.APP_ID || .*/appId: process.env.APP_ID || '$APP_ID',/" ./$APP_NAME/index.js
+sudo sed -i -e "s/APP_ID/$APP_ID/" "./$APP_NAME/parse-dashboard-config.json"
+sudo sed -i -e "s/APP_NAME/$APP_NAME/" "./$APP_NAME/parse-dashboard-config.json"
+sudo sed -i -e "s/DOMAIN/$DOMAIN/" "./$APP_NAME/parse-dashboard-config.json"
+sudo sed -i -e "s/APP_NAME/$APP_NAME/" "./$APP_NAME/dashboard-running.json"
+sudo sed -i -e "s/localhost:1337/app.$DOMAIN/" "./$APP_NAME/index.js"
+sudo sed -i -e "s/http/https/" "./$APP_NAME/index.js"
 MASTER_KEY=`pwgen -s 26 1`
-sudo sed -i "s/masterKey: process.env.MASTER_KEY || .*/masterKey: process.env.MASTER_KEY || '$MASTER_KEY',/" ~/$APP_NAME/index.js
-sudo sed -i -e "s/MASTER_KEY/$MASTER_KEY/" "~/$APP_NAME/parse-dashboard-config.json"
+sudo sed -i "s/masterKey: process.env.MASTER_KEY || .*/masterKey: process.env.MASTER_KEY || '$MASTER_KEY',/" ./$APP_NAME/index.js
+sudo sed -i -e "s/MASTER_KEY/$MASTER_KEY/" "./$APP_NAME/parse-dashboard-config.json"
 
 PASS=`pwgen -s 9 1`
-sudo sed -i -e "s/PASS/$PASS/" "~/$APP_NAME/parse-dashboard-config.json"
+sudo sed -i -e "s/PASS/$PASS/" "./$APP_NAME/parse-dashboard-config.json"
 tput setaf 2; echo 'Enable pm2';
 echo
 tput sgr0
@@ -103,7 +103,5 @@ echo
 echo
 tput setaf 3;  echo "Installation & configuration succesfully finished."
 echo
-echo "Twitter @TeamKloudboy"
-echo "E-mail: support@kloudboy.com"
-echo "Bye! Your boy KLOUDBOY!"
+
 tput sgr0
